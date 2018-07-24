39383ddfcde236543fc2116269a0f962eb61e3b9
NgariMutugi <NgariMutugi@github.com>
2018-07-24 20:18:15 +0300

    Added templates and created app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+ # we need to create an instance of the Flask class
+
+app = Flask(__name__)
+
+@app.route('/')
+
+
+def index(): 
+        return render_template('home.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
