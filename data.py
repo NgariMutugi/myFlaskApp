4db45968e21dd056177a5f738e9f3e1e2570252f
NgariMutugi <NgariMutugi@github.com>
2018-07-27 12:25:02 +0300

    templates
    added template layouts
    crated a data file data.py
    lendered data from data.py

---
+++ data.py
@@ -0,0 +1,34 @@
+def articles_fn():
+    articles =[
+        {
+        'id': 1,
+        'title': 'Article One',
+        'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. '
+                'Aspernatur at atque consequatur cum, deleniti'
+                'deserunt dicta eos excepturi fugit harum illum '
+                'labore laborum nobis quam quia quibusdam rerum sed voluptate',
+        'author': 'Eustus Hubert',
+        'date': '27-07-2018'
+            },
+        {
+        'id': 2,
+        'title': 'Article Two',
+        'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. '
+                'Aspernatur at atque consequatur cum, deleniti'
+                'deserunt dicta eos excepturi fugit harum illum '
+                'labore laborum nobis quam quia quibusdam rerum sed voluptate',
+        'author': 'Kara Hasmita',
+        'date': '27-07-2018'
+            },
+        {
+        'id': 3,
+        'title': 'Article Three',
+        'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. '
+                'Aspernatur at atque consequatur cum, deleniti'
+                'deserunt dicta eos excepturi fugit harum illum '
+                'labore laborum nobis quam quia quibusdam rerum sed voluptate',
+        'author': 'John Doe',
+        'date': '27-07-2018'
+            }
+        ]
+    return articles
